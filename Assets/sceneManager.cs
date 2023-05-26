b772f760f2fe465b7b18ef979a7bba40b3664f04
Oded Morag <odedmorag45@gmail.com>
2023-05-26 15:14:52 +0300

    Update sceneManager.cs

--- Assets/sceneManager.cs
+++ Assets/sceneManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class sceneManager : MonoBehaviour
 {
@@ -10,10 +11,12 @@ public class sceneManager : MonoBehaviour
     [SerializeField] tankManager tankManager;
     [SerializeField] artilleryManager artilleryManager;
     [SerializeField] textManager textManager;
-    
+    [SerializeField] Vector2 raycast = new Vector2
+    public GameObject battlefield;
     public Camera ARcamera;
     public ARRaycastManager raycastManager;
 
+    private ARRaycastHit hit;
 
     private Boolean readyFlag = false;
 
@@ -22,8 +25,14 @@ public class sceneManager : MonoBehaviour
     {
         while (!readyFlag)
         {
-            Ray ray = ARcamera.ScreenPointToRay(new Vector3(-0.5f, -0.5f, 0.0f));
-            if()
+
+            //Ray ray = ARcamera.ScreenPointToRay(new Vector3(-0.5f, -0.5f, 0.0f));
+            if (raycastManager.Raycast(ray, hit, TrackableType.Planes)
+            {
+                Pose pose= hit.pose;
+                Instantiate(battlefield, pose.position, pose.rotation);
+                readyFlag = true;
+            }
         }
         
 
