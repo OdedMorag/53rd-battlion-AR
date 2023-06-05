b96db1e58bab16967b96dbc02d1d834c81295fe2
HaimHH <79162474+HaimHH@users.noreply.github.com>
2023-06-05 07:58:57 +0300

    change textbox to Hebrew textbox

--- Assets/Assets/Scripts/informationTextManager.cs
+++ Assets/Assets/Scripts/informationTextManager.cs
@@ -12,13 +12,13 @@ public class informationTextManager : MonoBehaviour
     [SerializeField]
     private Button backButton;
     [SerializeField]
-    private TextMeshProUGUI text;
+    private TMP_Text text;
 
     private int textCounter = -1;
     string[] englishTexts = { "Saturday afternoon October 1973 Yom Kippur War", "The 53rd Battalion of the 188th Brigade, under the command of Lt. Col. Oded Erez, fought to contain the Syrian army",
         "During the first day of fighting, the battery bombarded the Syrian enemy and provided supporting fire to the Israeli infantry positions.", "" };
     string[] hebrewTexts = { "שבת אחר הצהריים אוקטובר 1973 מלחמת יום כיפור", "גדוד 53 של חטיבה 188, בפיקודו של סא\"ל עודד ארז, נלחם לבלימת הצבא הסורי",
-        "\r\n135 / 5,000\r\nתוצאות התרגום\r\nתוצאת התרגום\r\nבמהלך היום הראשון ללחימה הפציצה הסוללה את האויב הסורי וסיפקה אש תומכת לעמדות החי\"ר הישראליות.", "" };
+        "במהלך היום הראשון ללחימה הפציצה הסוללה את האויב הסורי וסיפקה אש תומכת לעמדות החי\"ר הישראליות", "" };
 
 
     public int textLen()
