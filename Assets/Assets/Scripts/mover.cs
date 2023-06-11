d85e0136ffb317d2fcd30f342fe63f8dfb7271ca
Oded Morag <odedmorag45@gmail.com>
2023-06-11 18:17:26 +0300

    shot

--- Assets/Assets/Scripts/mover.cs
+++ Assets/Assets/Scripts/mover.cs
@@ -7,11 +7,20 @@ public class mover : MonoBehaviour
 {
     [SerializeField] private path path;
     [SerializeField] private float movmentspeed = 1.0f;
+    [SerializeField] GameObject muzzle;
+    [SerializeField] GameObject shotVFX;
+    [SerializeField] float showMINdelay = 1.0f;
+    [SerializeField] float showMAXdelay = 12.0f;
     private Vector3 destination;
     
     int waypointIndex = 0;
     Boolean moveFlag = false;
+    AudioSource muzzleAudio;
 
+    private void Start()
+    {
+        muzzleAudio = muzzle.GetComponent<AudioSource>();
+    }
     private void Update()
     {
         if(moveFlag==true)
@@ -31,7 +40,25 @@ public class mover : MonoBehaviour
     {
         yield return new WaitForSeconds(UnityEngine.Random.Range(minDelay, maxDelay));
         if (moveFlag == false)
+        {
             moveFlag = true;
+            StartCoroutine(shotBehavior());
+        }
+            
         destination = path.GetWayPoint(waypointIndex);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(4.0f);
+        GetComponent<AudioSource>().Pause();
+    }
+
+    IEnumerator shotBehavior()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(showMINdelay, showMAXdelay));
+            var myExplosion = Instantiate(shotVFX, muzzle.transform);
+            muzzleAudio.Play();
+            Destroy(myExplosion.gameObject, 1);
+        }
     }
 }
